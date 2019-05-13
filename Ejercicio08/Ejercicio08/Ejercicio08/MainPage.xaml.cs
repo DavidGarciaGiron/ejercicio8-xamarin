8a05a0d61abc9e76ec15c8add316824ae5ce8fb5
sagitaforever <david.garcia@tecsup.edu.pe>
2019-05-13 16:44:08 -0500

    first commit

---
+++ Ejercicio08/Ejercicio08/Ejercicio08/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Ejercicio08.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio08
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SimpleDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InteractiveDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CommandDemo());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Operaciones());
+
+            };
+        }
+    }
+}
